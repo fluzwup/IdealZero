bb10fbc6c8f8dab9475ac3190fe97f0c2373a409
fluzwup <scot.alexander@gmail.com>
2018-10-09 08:03:34 -0500

    Modifying ideal zero to use ExtBal.js

--- ExtBal.js
+++ ExtBal.js
@@ -1,88 +1,133 @@
 // inputs are muzzle velocity in feet per second, ballistic coefficient (G1 model),
 // sight height above bore in inches, and elevation in minutes of arc
 // outputs are vectors of drops, velocities, time, and drift at every foot distance
-		function RunExtBal(mvel, bc, sightHt, moa, maxRange, wvel, trajectory, velocities, times, drifts)
+// if maxht and maxdrp are greater than zero, then the simulation will stop as soon
+// as the evelvation goes over maxht is below maxdrp (after it starts to drop)
+function RunExtBal(mvel, bc, sightHt, moa, maxRange, wvel, trajectory, velocities, times, drifts, dispersion, maxht, maxdrp)
+{ 
+	// clear output arrays
+	trajectory.length = 0;
+	velocities.length = 0;
+	times.length = 0;
+	drifts.length = 0;
+
+	// size output arrays
+	trajectory.length = maxRange;
+	velocities.length = maxRange;
+	times.length = maxRange;
+	drifts.length = maxRange;
+
+	// calculate elevation in radians from MOA
+	var elev = moa / (180 * 60) * 3.14;
+
+	// delta time, the interval at which the bullet motion is calcuated
+	var dt = 0.1 / mvel;	// 10 calculationis per foot
+
+	// the "fudge factors" for supersonic and subsonic flight
+	// these numbers are to fit a G1 drag model
+	var superfudge = 7900 / dt;  // empirically determined drag value for supersonic speeds
+	var subfudge = 180000 / dt;  // for subsonic speeds
+	var vfactor = 0.87;		  // empirically determined correction for bullet drop
+	var wfactor = 1.9;		   // empirically determined value to calculate wind drift
+
+	// apply fudge factors to ballistic coefficient, so drag can be calculated
+	var superbc = superfudge * bc;
+	var subbc = subfudge * bc;
+	 
+	var vvel = 0;	// negative vvel is moving up
+	var drop = 0;
+	// wind drift amount and velocity
+	var drift = 0;
+	var vdrift = 0;
+
+	// set up initial position and velocities
+	vvel = -mvel * Math.sin(elev);
+	hvel = mvel * Math.cos(elev);
+	drop = sightHt / 12;
+	var dist = 0;
+	var highest = drop;
+	 
+	// horizontal interval at which we store trajectory information (in yards)
+	var check = 10; 
+	// Set up flags to denote what state the projectile is in
+	var minimum = 0;
+	var mpbr = 0;
+	var nearZero = 0;
+	var farZero = 0;
+	 
+	// go for up to five seconds, calculating the flight of the projectile
+	var t;
+	for(t = 0; t < 5; t = t + dt)
+	{
+		// start by storing the distance for each foot (it's OK that we overwrite a bunch,
+		//	we do NOT want to skip any or it will mess up the chart)
+		trajectory[Math.round(dist)] = drop * 12;  // convert to inches for storage
+		velocities[Math.round(dist)] = hvel;
+		times[Math.round(dist)] = t;
+		drifts[Math.round(dist)] = drift * 12;  // convert to inches for storage
+		 
+		// add acceleration due to gravity, 32.2 f/s^2
+		vvel = vvel + 32.2 * dt;
+		
+		// account for wind drift
+		// add current drift amount
+		drift += vdrift * dt;
+		// calculate wind/bullet differential
+		var deltavdrift = wvel - vdrift;
+		// calculate acceleration based on velocity differential
+		var accel = Math.sqrt(deltavdrift) * wfactor;
+		// calculate new drift velocity based on acceleration
+		vdrift += accel * dt;
+		 
+		// account for drag, supersonic or subsonic; speed of sound is about 1125 fps
+		// just in case there's significant vertical velocity, add some drag there, too
+		if(hvel > 1125)
 		{
-			// calculate elevation in radians from MOA
-			var elev = moa / (180 * 60) * 3.14;
-		
-			// delta time, the interval at which the bullet motion is calcuated
-			var dt = 0.1 / mvel;	// 10 calculationis per foot
-		
-			// the "fudge factors" for supersonic and subsonic flight
-			// these numbers are to fit a G1 drag model
-			var superfudge = 7900 / dt;  // empirically determined drag value for supersonic speeds
-			var subfudge = 180000 / dt;  // for subsonic speeds
-			var vfactor = 0.87;          // empirically determined correction for bullet drop
-			var wfactor = 1.9;           // empirically determined value to calculate wind drift
-		
-			// apply fudge factors to ballistic coefficient, so drag can be calculated
-			var superbc = superfudge * bc;
-			var subbc = subfudge * bc;
-			 
-			var vvel = 0;
-			var drop = 0;
-			// wind drift amount and velocity
-			var drift = 0;
-			var vdrift = 0;
-		
-			// set up initial position and velocities
-			vvel = -mvel * Math.sin(elev);
-			hvel = mvel * Math.cos(elev);
-			drop = sightHt / 12;
-			var dist = 0;
-			var highest = drop;
-			 
-			// horizontal interval at which we store trajectory information (in yards)
-			var check = 10; 
-			// Set up flags to denote what state the projectile is in
-			var minimum = 0;
-			var mpbr = 0;
-			var nearZero = 0;
-			var farZero = 0;
-			 
-			// go for up to two and a half seconds, calculating the flight of the projectile
-			var t;
-			for(t = 0; t < 2.5; t = t + dt)
-			{
-				// start by storing the distance for each foot (it's OK that we overwrite a bunch,
-				//	we do NOT want to skip any or it will mess up the chart)
-				trajectory[Math.round(dist)] = drop * 12;  // convert to inches for storage
-				velocities[Math.round(dist)] = hvel;
-				times[Math.round(dist)] = t;
-				drifts[Math.round(dist)] = drift * 12;  // convert to inches for storage
-				 
-				// add acceleration due to gravity, 32.2 f/s^2
-				vvel = vvel + 32.2 * dt;
-				
-				// account for wind drift
-				// add current drift amount
-				drift += vdrift * dt;
-				// calculate wind/bullet differential
-				var deltavdrift = wvel - vdrift;
-				// calculate acceleration based on velocity differential
-				var accel = Math.sqrt(deltavdrift) * wfactor;
-				// calculate new drift velocity based on acceleration
-				vdrift += accel * dt;
-				 
-				// account for drag, supersonic or subsonic; speed of sound is about 1125 fps
-				// just in case there's significant vertical velocity, add some drag there, too
-				if(hvel > 1125)
-				{
-					hvel = hvel - (hvel * hvel) / superbc;
-				  vvel = vvel - (vvel * vvel) / superbc;
-				}
-				else
-				{
-					hvel = hvel - (hvel * hvel) / subbc;
-				  vvel = vvel - (vvel * vvel) / subbc;
-				}
-				 
-				// update bullet position
-				drop = drop + vvel * dt * vfactor;
-				dist = dist + hvel * dt;
-		
-				// check for terminating conditions
-				if(dist > maxRange) t = 100;
-			}
+			hvel = hvel - (hvel * hvel) / superbc;
+			vvel = vvel - (vvel * vvel) / superbc;
 		}
+		else
+		{
+			hvel = hvel - (hvel * hvel) / subbc;
+			vvel = vvel - (vvel * vvel) / subbc;
+		}
+		 
+		// update bullet position
+		drop = drop + vvel * dt * vfactor;
+		dist = dist + hvel * dt;
+
+		// calculate dispersion, look for terminating conditions
+			
+		// find the maximum probable +/- error at this range
+		// first, find 1 MOA error (1" at 95.5 yards)
+		var err = (dist / 3) / 95.5;
+		// now multiply by dispersion / 2 to get radis
+		err *= (dispersion / 2);
+		// now convert to feet
+		err /= 12;
+		var lowest = drop + err;
+		var highest = -drop + err;
+
+		// check max height
+		if(maxht > 0 && highest > maxht) 
+		{
+			t = 100;  // exceeded height
+			return -1;	// out of bounds, so this trajectory fails
+		}
+		// check max drop, but only if we're moving down (in case sight height is more than radius)
+		if(maxdrp > 0 && lowest > maxdrp && vvel > 0) 
+		{	
+			t = 100;  // exceeded drop
+			return dist; // max point blank range at this elevation
+		}
+			
+		// check for max range
+		if(dist > maxRange) 
+		{
+			t = 100;			// exceeded range
+			return maxRange;
+		}
+
+	}
+	return -1;		// this shouldn't happen, we should hit a terminating condition above
+}
--- IdealZero.js
+++ IdealZero.js
@@ -11,18 +11,10 @@
 		var sightHt = document.getElementById("sightHt").value;
 		var diameter = document.getElementById("targetDia").value;
 		var dispersion = document.getElementById("dispersion").value;
-		var moa = document.getElementById("elevation").value;
-		var wvel = document.getElementById("wind").value * 5280 / 3600; // mph to fps conversion
+		var wvel = 0;	// wind is ignored in this
 		var temp = document.getElementById("temp").value; // degrees F
 		var pres = document.getElementById("pres").value; // inches of mercury
-		var balChartSpacing;
-		if(document.getElementById("yd_25").checked) balChartSpacing = 25; 
-		if(document.getElementById("yd_50").checked) balChartSpacing = 50; 
-		if(document.getElementById("yd_100").checked) balChartSpacing = 100; 
-		var milChartSpacing;
-		if(document.getElementById("mil_1.0").checked) milChartSpacing = 1.0; 
-		if(document.getElementById("mil_0.5").checked) milChartSpacing = 0.5; 
-		if(document.getElementById("mil_0.25").checked) milChartSpacing = 0.25; 
+		var balChartSpacing = 5; 
 		
 		// converte to absolute
 		var abstemp = 459 + parseInt(temp);
@@ -33,8 +25,8 @@
 		bc *= 29.53 / pres;
 		outputDoc.innerHTML += "Corrected ballisic coefficient " + bc + "\n";
 
-		//	convert from diameter to radius
-		var radius = diameter / 2;
+		//	convert from diameter to radius, and inches to feet
+		var radius = diameter / 2 / 12;
 
 		// calculate range where dispersion equals target size
 		// 1" = 1 moa * 95.5 yards, convert to feet
@@ -50,27 +42,59 @@
 		var times = new Array();
 		var drifts = new Array();
 
-		trajectory.length = maxRange;
-		velocities.length = maxRange;
-		times.length = maxRange;
-		drifts.length = maxRange;
+		// loop through at various angles of elevation to find the best one
+		// start way too high 
+		var moaMax = 20.0;
+		var moaMin = 0;
+		var done = false;
 
-		RunExtBal(mvel, bc, sightHt, moa, maxRange, wvel, trajectory, velocities, times, drifts, 0, 0);
+		// while finalRange is less than zero, then we're too high, and need to lower the elevation
+		// we want the highest elevation possible without a less than zero result, that gives us
+		// the maximum point blank range
+		while(done == false)
+		{
+			var moa = (moaMax + moaMin) / 2;
+			var thisRange = RunExtBal(mvel, bc, sightHt, moa, maxRange, wvel, 
+					trajectory, velocities, times, drifts, dispersion, radius, radius);
+
+			//console.log(moaMin + ", " + moa + ", " + moaMax + ", " + thisRange);
+
+			// negative range is too high, so drop the elevation
+			if(thisRange < 0)
+			{
+				// lower the elevation
+				moaMax = moa;
+			}
+			// if we get a range back, we're not too high, so raise the elevation,
+			if(thisRange >= 0)
+			{
+				moaMin = moa;
+			}
+
+			if(moaMax - moaMin < 0.01) done = true;
+		}
+
+		// moaMin should be the lowest that gave us a full run, so run it again
+		var finalRange = RunExtBal(mvel, bc, sightHt, moaMin, maxRange, wvel, 
+					trajectory, velocities, times, drifts, dispersion, radius, radius);
 		
 		outputDoc.innerHTML += "\n\nTrajectory chart for " + temp + " F and " + pres + " in of mercury\n";
 		outputDoc.innerHTML += "Muzzle velocity " + Math.round(mvel) + " fps BC " + baseBC + 
 					" sight height " + sightHt + " in\n";
-		outputDoc.innerHTML += "Maximum range for " + diameter + " in group is at " + Math.round(maxRange / 3) + " yds.\n";
-		outputDoc.innerHTML += "Elevation " + Math.round(moa * 100) / 100 + " moa and crosswind of ";
-		outputDoc.innerHTML += wvel * 3600 / 5280 + " mph\n";
+		outputDoc.innerHTML += "Maximum point blank range for " + diameter + " in target is at " + Math.round(finalRange / 3) + " yds.\n";
+		outputDoc.innerHTML += "Elevation " + Math.round(moa * 100) / 100 + " moa\n";
 		
 		var last = trajectory[0]; 
 		var nearZero = 0;
 		var farZero = 0;
-		var yRange = 0;
-		for(r = 0; r <= maxRange; r++)
+		var yRange = Math.max(sightHt, diameter / 2);
+		for(r = 0; r <= finalRange; r++)
 		{
-			if(Math.abs(trajectory[r]) > yRange) yRange = Math.abs(trajectory[r]);
+			// convert to multiples of 95.5 yards (where 1 MOA = 1 inch)
+			var error = (r / 3) / 95.5;
+			// convert to radius of error in inches
+			error *= dispersion / 2;
+
 			if(last > 0 && trajectory[r] <= 0)
 			{
 				nearZero = r;
@@ -85,10 +109,10 @@
 
 			last = trajectory[r];
 		}
-	
-		outputDoc.innerHTML += "Range\tDrop\tHold \tTime\tDrift\tWindage\tVelocity\n";
-		outputDoc.innerHTML += "(yds)\t(in)\t(mil)\t(ms)\t(in) \t(mil)  \t(fps)   \n";
-		for(r = 0; r <= maxRange; r += balChartSpacing * 3)
+
+		outputDoc.innerHTML += "Range\tDrop\tHold \tTime\tVelocity\n";
+		outputDoc.innerHTML += "(yds)\t(in)\t(mil)\t(ms)\t(fps)   \n";
+		for(r = 0; r <= finalRange; r += balChartSpacing * 3)
 		{
 			outputDoc.innerHTML += r / 3 + "\t";
 			outputDoc.innerHTML += Math.round(trajectory[r] * 10) / 10 + "\t";
@@ -98,29 +122,7 @@
 				angle = trajectory[r] / (r * 12.0) * 1000;		// approximately the angle in milliradians
 			outputDoc.innerHTML += Math.round(angle * 10) / 10 + "\t";
 			outputDoc.innerHTML += Math.round(1000 * (times[r])) + "\t";
-			outputDoc.innerHTML += Math.round(drifts[r]) + "\t";
-			if(r > 0)
-				angle = drifts[r] / (r * 12.0) * 1000;		// approximately the angle in milliradians
-			outputDoc.innerHTML += Math.round(10 * angle) / 10 + "\t";
 			outputDoc.innerHTML += Math.round(velocities[r]) + "\n";
-		}
-
-		outputDoc.innerHTML += "\n\nMil-dot hold-over range chart\n";
-		outputDoc.innerHTML += "Hold\tRange\tTime\n";
-		outputDoc.innerHTML += "(mil)\t(yds)\t(ms)\n";
-		// go through the trajectory a yard at a time looking for quarter radian increments in drop
-		var fractions = 1 / milChartSpacing;		// number of divisions per milliradtion; 2 is half mils, 4 is quarter mils
-		var fractionalMils = 1; 	// number of fractions to look for
-		for(r = farZero; r < maxRange; r += 3)
-		{
-			// drop is in inches, r in feet, get angle in MOA
-			mrs = 1000 * trajectory[r] / (r * 12.0); 	// approximately the angle in radians times 1000 to get milliradians
-			// if we've passed another milliradian, then print the range and look for the next milliradian
-			if(mrs > fractionalMils / fractions)
-			{
-				outputDoc.innerHTML += fractionalMils / fractions + "\t" + Math.round(r / 3) + "\t" + Math.round(1000 * times[r]) + "\n";
-				fractionalMils += 1;
-			}
 		}
 
 		var outputCanvas = document.getElementById("drawArea");
@@ -135,20 +137,22 @@
 		// start at zero
 		gc.moveTo(0, 250 + trajectory[0] * 500 / (yRange * 2));
 		// draw top line
-		for(r = 1; r < maxRange - 1; r++) 
+		for(r = 1; r < finalRange - 1; r++) 
 		{
 			// calculate group radius at this range to find highest possible point of impact 
-			var error = (r / 3) / 95.5 * (dispersion / 2);
+			var error = (r / 3) / 95.5;
+			error *= (dispersion / 2);
 			tMax = trajectory[r] + error;
-			gc.lineTo(r * 750 / maxRange, 250 + tMax * 500 / (yRange * 2));
+			gc.lineTo(r * 750 / finalRange, 250 + tMax * 500 / (yRange * 2));
 		}
 		// draw bottom line back to origin
 		for(; r > 0; r--) 
 		{
 			// calculate group radius at this range to find lowest possible point of impact 
-			var error = (r / 3) / 95.5 * (dispersion / 2);
+			var error = (r / 3) / 95.5;
+			error *= (dispersion / 2);
 			tMin = trajectory[r] - error;
-			gc.lineTo(r * 750 / maxRange, 250 + tMin * 500 / (yRange * 2));
+			gc.lineTo(r * 750 / finalRange, 250 + tMin * 500 / (yRange * 2));
 		}
 
 		// close and fill
@@ -158,24 +162,24 @@
 		// draw ideal trajectory
 		gc.strokeStyle = "black";
 		gc.moveTo(0, 250 + trajectory[0] * 500 / (yRange * 2));
-		for(r = 1; r < maxRange - 1; r++) 
-				gc.lineTo(r * 750 / maxRange, 250 + trajectory[r] * 500 / (yRange * 2));
+		for(r = 1; r < finalRange - 1; r++) 
+				gc.lineTo(r * 750 / finalRange, 250 + trajectory[r] * 500 / (yRange * 2));
 		gc.stroke();
 
 		// draw milliradian marks on trajectory
 		gc.strokeStyle = "black";
 		var mils = 1; 	// number of fractions to look for
-		for(r = farZero; r < maxRange; r ++)
+		for(r = farZero; r < finalRange; r ++)
 		{
 			// drop is in inches, r in feet, get angle in MOA
 			mrs = 1000 * trajectory[r] / (r * 12.0); 	// approximately the angle in radians times 1000 to get milliradians
 			// if we've passed another milliradian, then print the range and look for the next milliradian
 			if(mrs > mils)
 			{
-				gc.moveTo(r * 750 / maxRange - 5, 250 + trajectory[r] * 500 / (yRange * 2));
-				gc.lineTo(r * 750 / maxRange + 5, 250 + trajectory[r] * 500 / (yRange * 2));
-				gc.moveTo(r * 750 / maxRange, 250 + trajectory[r] * 500 / (yRange * 2) - 5);
-				gc.lineTo(r * 750 / maxRange, 250 + trajectory[r] * 500 / (yRange * 2) + 5);
+				gc.moveTo(r * 750 / finalRange - 5, 250 + trajectory[r] * 500 / (yRange * 2));
+				gc.lineTo(r * 750 / finalRange + 5, 250 + trajectory[r] * 500 / (yRange * 2));
+				gc.moveTo(r * 750 / finalRange, 250 + trajectory[r] * 500 / (yRange * 2) - 5);
+				gc.lineTo(r * 750 / finalRange, 250 + trajectory[r] * 500 / (yRange * 2) + 5);
 				mils += 1;
 			}
 		}
@@ -215,17 +219,17 @@
 	
 		// determine horizontal grid spacing
 		step = 30;
-		if(maxRange > 450) step = 60;
-		if(maxRange > 900) step = 120;
-		if(maxRange > 3000) step = 300;
+		if(finalRange > 450) step = 60;
+		if(finalRange > 900) step = 120;
+		if(finalRange > 3000) step = 300;
 	
-		for(r = 0; r < maxRange; r += step)
+		for(r = 0; r < finalRange; r += step)
 		{
-			gc.moveTo(r * 750 / maxRange, 0);
-			gc.lineTo(r * 750 / maxRange, 500);
+			gc.moveTo(r * 750 / finalRange, 0);
+			gc.lineTo(r * 750 / finalRange, 500);
 			gc.stroke();
 			// mark range in yards
-			if(r > 0) gc.fillText(r / 3, r * 750 / maxRange, 480);
+			if(r > 0) gc.fillText(r / 3, r * 750 / finalRange, 480);
 		}
 
 		// label axes
